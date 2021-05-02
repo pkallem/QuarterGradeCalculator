5a1af682b878c9dad207c39e9f0bba5105249686
Pranav Kallem <66687154+pkallem@users.noreply.github.com>
2021-05-02 16:00:33 -0400

    Add files via upload

---
+++ Form1-11.vb
@@ -0,0 +1,19 @@
+﻿Public Class frmAverageGrade
+    Private Sub btnCalculateTheAverage_Click(sender As Object, e As EventArgs) Handles btnCalculateTheAverage.Click
+        Dim dblGradeYouEntered As Double
+        Dim dblAverage As Double
+
+        For intCount = 1 To 5
+
+            dblGradeYouEntered = Convert.ToDouble(InputBox("Enter your first grade here", "Enter your numbers"))
+
+            lblLabel.Text &= "Grade Entered: " & dblGradeYouEntered & vbCrLf
+            dblAverage += dblGradeYouEntered
+
+        Next intCount
+
+
+
+        lblLabel.Text &= "Grade Average:  " & dblAverage / 5
+    End Sub
+End Class
